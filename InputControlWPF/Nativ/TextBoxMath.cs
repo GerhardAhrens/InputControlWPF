9ad160044eeb8a02b8eb10d0997b61843e433e73
Gerhard Ahrens <gerhard.ahrens@pta.de>
2024-02-02 14:47:53 +0100

    *

--- InputControlWPF/Nativ/TextBoxMath.cs
+++ InputControlWPF/Nativ/TextBoxMath.cs
@@ -33,12 +33,12 @@ namespace InputControlWPF.InputControls
         private const string ICON_COPY = "M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z";
         private const string ICON_PASTE = "M19,20H5V4H7V7H17V4H19M12,2A1,1 0 0,1 13,3A1,1 0 0,1 12,4A1,1 0 0,1 11,3A1,1 0 0,1 12,2M19,2H14.82C14.4,0.84 13.3,0 12,0C10.7,0 9.6,0.84 9.18,2H5A2,2 0 0,0 3,4V20A2,2 0 0,0 5,22H19A2,2 0 0,0 21,20V4A2,2 0 0,0 19,2Z";
         private const string ICON_DELETE = "M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z";
-        private const string ICON_CLOCK = "M12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22C6.47,22 2,17.5 2,12A10,10 0 0,1 12,2M12.5,7V12.25L17,14.92L16.25,16.15L11,13V7H12.5Z";
         private const string ICON_ADD = "M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z";
         private const string ICON_EQUALS = "M19,10H5V8H19V10M19,16H5V14H19V16Z";
         private const string ICON_MINUS = "M19,13H5V11H19V13Z";
         private const string ICON_MULT = "M11,3H13V10.27L19.29,6.64L20.29,8.37L14,12L20.3,15.64L19.3,17.37L13,13.72V21H11V13.73L4.69,17.36L3.69,15.63L10,12L3.72,8.36L4.72,6.63L11,10.26V3Z";
         private const string ICON_DIV = "M7 21L14.9 3H17L9.1 21H7Z";
+        private const string ICON_PERCENT = "M18.5 3.5L20.5 5.5L5.5 20.5L3.5 18.5L18.5 3.5M7 4C8.66 4 10 5.34 10 7C10 8.66 8.66 10 7 10C5.34 10 4 8.66 4 7C4 5.34 5.34 4 7 4M17 14C18.66 14 20 15.34 20 17C20 18.66 18.66 20 17 20C15.34 20 14 18.66 14 17C14 15.34 15.34 14 17 14M7 6C6.45 6 6 6.45 6 7C6 7.55 6.45 8 7 8C7.55 8 8 7.55 8 7C8 6.45 7.55 6 7 6M17 16C16.45 16 16 16.45 16 17C16 17.55 16.45 18 17 18C17.55 18 18 17.55 18 17C18 16.45 17.55 16 17 16Z";
 
         private static Regex regexDisallowedInteger = new Regex(@"[^0-9-]+");  // matches disallowed text
         private static Regex regexDisallowedFloat = new Regex(@"[^0-9-+.,e]+");  // matches disallowed text
@@ -246,12 +246,6 @@ namespace InputControlWPF.InputControls
                 deleteMenu.Icon = this.GetPathGeometry(ICON_DELETE);
                 WeakEventManager<MenuItem, RoutedEventArgs>.AddHandler(deleteMenu, "Click", this.OnDeleteMenu);
                 textBoxContextMenu.Items.Add(deleteMenu);
-
-                MenuItem setDateMenu = new MenuItem();
-                setDateMenu.Header = "Setze Datum";
-                setDateMenu.Icon = this.GetPathGeometry(ICON_CLOCK);
-                WeakEventManager<MenuItem, RoutedEventArgs>.AddHandler(setDateMenu, "Click", this.OnSetDateMenu);
-                textBoxContextMenu.Items.Add(setDateMenu);
             }
 
             return textBoxContextMenu;
@@ -318,7 +312,7 @@ namespace InputControlWPF.InputControls
 
 
         #region Math
-        private enum EOperation { Add, Subtract, Multiply, Divide }
+        private enum EOperation { Add, Subtract, Multiply, Divide, Percent }
         private EOperation operation = EOperation.Add;
         private ContextMenu menuMath = null;
         private MenuItem miOperand = null;
@@ -329,7 +323,7 @@ namespace InputControlWPF.InputControls
             if (menuMath != null)
                 return false;
 
-            if (text == "+" || text == "-" || text == "*" || text == "/")
+            if (text == "+" || text == "-" || text == "*" || text == "/" || text == "%")
             {
                 if (text == "-" && CaretIndex == 0)  // negative sign in front of number
                     return false;
@@ -359,6 +353,11 @@ namespace InputControlWPF.InputControls
                 {
                     operation = EOperation.Divide;
                     miOperand.Icon = this.GetPathGeometry(ICON_DIV);
+                }
+                else if (text == "%")
+                {
+                    operation = EOperation.Percent;
+                    miOperand.Icon = this.GetPathGeometry(ICON_PERCENT);
                 }
 
                 miResult = new MenuItem();
@@ -523,6 +522,11 @@ namespace InputControlWPF.InputControls
                                 miResult.IsEnabled = true;
                             }
                         }
+                        break;
+                    case EOperation.Percent:
+                        result = (dop1 / dop2) * 100;
+                        miResult.Header = result.ToString();
+                        miResult.IsEnabled = true;
                         break;
                 }
             }
