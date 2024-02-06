f8d9eb60908ad32f8a68cf7ea638b7adb7cdfab5
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2024-02-06 20:45:01 +0100

    *

---
+++ InputControlWPF/Nativ/TextBoxNumericUpDown.xaml.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * https://www.codeproject.com/Articles/139629/A-Numeric-Up-Down-Control-for-WPF
+ */
+
+namespace InputControlWPF.InputControls
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaktionslogik für TextBoxNumericUpDown.xaml
+    /// </summary>
+    public partial class TextBoxNumericUpDown : UserControl
+    {
+        public TextBoxNumericUpDown()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
